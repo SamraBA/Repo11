6e15269529d8b8731ba265979d4d0a4b11794802
SamraBA <90388771+SamraBA@users.noreply.github.com>
2021-09-24 10:09:32 +0200

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,11 +1,11 @@
 // Create a temps array here
 var temps = [
-    [31, 32, 19, 37],
-    [29, 27, 55, 36],
-    [17, 27, 42, 46],
-    [29, 52, 21, 64],
-    [91, 27, 31, 61]
-  ];
+  monday = [31, 32, 19, 37], 
+  tuesday = [29, 27, 55, 36], 
+  wednesday = [17, 27, 42, 46], 
+  thursday = [29, 52, 21, 64], 
+  friday = [91, 27, 31, 61] 
+];
 // End of temps array
 function myArrayFunction(arr) {
     var newTemps = [...arr];
